c42d2751466d1ca1b1093f3718bdb60ecd8c5ccc
brad oyler <bradoyler@gmail.com>
2013-10-05 23:10:58 -0400

    rename

--- javascripts/app.js
+++ javascripts/app.js
@@ -639,7 +639,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
 
 
-  data.buffer.push("<div class=\"container\">\n  <div class=\"header\">\n    <h2><a href=\"/\">Ember.js Beats</a></h2>\n\n    <span>\n      <a href=\"https://twitter.com/gavinjoyce\">@gavinjoyce</a> |\n      <a href=\"https://github.com/GavinJoyce/ember-beats\">Source</a>\n    </span>\n  </div>\n\n  <br style=\"clear: both\" />\n\n  ");
+  data.buffer.push("<div class=\"container\">\n  <div class=\"header\">\n    <h2><a href=\"/\">BeatBox</a></h2>\n\n    <span>\n  <a href=\"https://github.com/bradoyler/ember-beats\">Source</a>\n    </span>\n  </div>\n\n  <br style=\"clear: both\" />\n\n  ");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
